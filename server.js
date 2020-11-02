09cb438cf24ed07cfe05a1f45fd3710c805cc447
xanderrr gitlab <xander.canedo@gmail.com>
2020-11-02 16:36:43 -0700

    pasring request URL and getting values

--- server.js
+++ server.js
@@ -8,11 +8,18 @@ var PORT = 8080;
 function handleRequest(request, response) {
 
   // Send the below string to the client when the user visits the PORT URL
-  response.end("It Works!! Path Hit: " + request.url);
+  // response.end("It Works!! Path Hit: " + request.url);
+  response.end("it works! " + parseUserID(request.url))
 }
 
 // TO DO :
 // I have to parse out the request.url stuff to identify the $$USER.ID params being sent and the $$SESSION.ID
+
+function parseUserID(requestURLGoesHere) {
+  var url_array = requestURLGoesHere.split('=')
+  console.log(url_array);
+
+}
 
 // Use the Node HTTP package to create our server.
 // Pass the handleRequest function to empower it with functionality.
