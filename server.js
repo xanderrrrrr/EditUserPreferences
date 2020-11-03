38e8f490e2ca6e47f18b1fa80c3ba3faaeb5221c
xanderrr gitlab <xander.canedo@gmail.com>
2020-11-02 17:19:25 -0700

    axios call with hardcoded session [redacted] works

--- server.js
+++ server.js
@@ -12,6 +12,31 @@ function handleRequest(request, response) {
   // do the parseURL elements function
   parseUrlElements(request.url)
 
+  // do the axios call 
+  axios
+  .get("https://xandercanedo.my.workfront.com/attask/api-unsupported/userpf/search?sessionID=")
+  .then(function(response) {
+    // if axios successful
+    console.log(response.data)
+  })
+  .catch(function(error) {
+    if (error.response) {
+      // The request was made and the server responded with a status code
+      // that falls out of the range of 2xx
+      console.log(error.response.data);
+      console.log(error.response.status);
+      console.log(error.response.headers);
+    } else if (error.request) {
+      // The request was made but no response was received
+      // `error.request` is an object that comes back with details pertaining to the error that occurred.
+      console.log(error.request);
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      console.log("Error", error.message);
+    }
+    console.log(error.config);
+  });
+
   // Send the below string to the client when the user visits the PORT URL
   response.end("it works! " )
 }
